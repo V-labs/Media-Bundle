1507ddd9330e0c60b2b6b8cfede97e8f31440759
Quentin Massias <quentin@v-labs.fr>
2019-12-17 17:43:10 +0100

    update for twig 3

--- Twig/Extension/MediaExtension.php
+++ Twig/Extension/MediaExtension.php
@@ -3,36 +3,43 @@
 namespace Vlabs\MediaBundle\Twig\Extension;
 
 use Symfony\Component\Asset\Packages;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 use Vlabs\MediaBundle\Manager\MediaManagerInterface;
 use Vlabs\MediaBundle\MediaInterface;
 
-class MediaExtension extends \Twig_Extension
+class MediaExtension extends AbstractExtension
 {
+    /**
+     * @var MediaManagerInterface
+     */
     private $mediaManager;
+
+    /**
+     * @var Packages
+     */
     private $packages;
 
+    /**
+     * MediaExtension constructor.
+     *
+     * @param MediaManagerInterface $manager
+     * @param Packages              $packages
+     */
     public function __construct(MediaManagerInterface $manager, Packages $packages)
     {
         $this->mediaManager = $manager;
-        $this->packages = $packages;
+        $this->packages     = $packages;
     }
 
     /**
-     * Returns the name of the extension.
-     *
-     * @return string The extension name
+     * @return array
      */
-    public function getName()
-    {
-        return 'vlabs_media';
-    }
-
-
     public function getFunctions()
     {
-        return array(
-            new \Twig_SimpleFunction('vlabs_media', array($this, 'media')),
-        );
+        return [
+            new TwigFunction('vlabs_media', [$this, 'media'])
+        ];
     }
 
     /**
@@ -40,16 +47,18 @@ class MediaExtension extends \Twig_Extension
      *
      * @param MediaInterface $media
      * @param array $options
+     *
      * @return string
      */
     public function media(MediaInterface $media, $options = [])
     {
         $absolute = isset($options['absolute']) ? $options['absolute'] : false;
-        $thumb = isset($options['thumb']) ? $options['thumb'] : false;
+        $thumb    = isset($options['thumb'])    ? $options['thumb']    : false;
 
         if ($this->mediaManager->hasMedia($media, $thumb)) {
             return $this->mediaManager->getUri($media, $absolute, $thumb);
         }
+
         return $this->packages->getUrl('/bundles/vlabsmedia/images/loader.svg');
     }
 }
